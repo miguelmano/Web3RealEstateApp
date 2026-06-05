ef85a15576890ec820f069e7657172c3c767f8b8
Miguel Mano <miguelccmano@gmail.com>
2026-06-05 18:59:33 +0100

    added comments to RealEstate.sol

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import {ERC721URIStorage, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+//RealEstate contract inherits from ERC721URIStorage, which is an extension of ERC721 that allows us to associate a URI with each token. This URI can point to metadata about the token, such as a description, image, etc. 
+//The RealEstate contract uses a counter to keep track of the token IDs and provides a mint function to create new tokens with a specified URI. It also has a totalSupply function to return the total number of tokens minted.
+//Each property can be represented as a unique token, and the metadata URI can contain information about the property, such as its location, size, and other relevant details. This allows for easy tracking and transfer of ownership of real estate properties on the blockchain.   
 contract RealEstate is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -18,7 +21,7 @@ contract RealEstate is ERC721URIStorage {
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
         
-        
+
         return newItemId;
     }
 
