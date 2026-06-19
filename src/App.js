8825a85711b55341c11ba88d85bc6759c1ba6302
miguel mano <miguelccmano@gmail.com>
2026-06-19 18:12:23 +0100

    frontend blockchain connection implementation

--- src/App.js
+++ src/App.js
@@ -14,6 +14,21 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  
+  //functions for reading and setting the account
+  //fetches component state
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+    setAccount(accounts[0])
+    console.log(accounts[0])
+  }
+
+  useEffect(() =>{
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
