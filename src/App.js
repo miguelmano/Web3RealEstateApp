05688e58aa9464e889ee2bdbad60fc2103aa18c3
miguel mano <miguelccmano@gmail.com>
2026-06-23 13:43:37 +0100

    navigation bar implementation

--- src/App.js
+++ src/App.js
@@ -21,9 +21,12 @@ function App() {
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
-    setAccount(accounts[0])
-    console.log(accounts[0])
+    
+    window.ethereum.on('accountsChanged', async () =>{
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account)
+    })
   }
 
   useEffect(() =>{
@@ -32,6 +35,8 @@ function App() {
 
   return (
     <div>
+
+      <Navigation account={account} setAccount={setAccount}/>
 
       <div className='cards__section'>
 
